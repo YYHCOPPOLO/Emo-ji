88beba399bad046ea1c2c7d056cc66df761db6a5
YYHCOPPOLO <1305403032@qq.com>
2022-09-04 11:26:42 +0800

    chore: style and code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,187 +1,373 @@
-import { Component, createSignal, For } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  For,
+  Match,
+  onMount,
+  Show,
+  Switch,
+} from "solid-js";
 import SelectButton from "./components/SelectButton";
 
+type SvgImageModule = typeof import("*.svg");
+type ImportModuleFunction = () => Promise<SvgImageModule>;
+type EmojiSlice = "head" | "eye" | "eyebow" | "mouth" | "detail";
+
+const tabs: EmojiSlice[] = ["head", "eye", "eyebow", "mouth", "detail"];
+let canvas: HTMLCanvasElement,
+  canvasSize = 640;
+
+/**
+ * get images path
+ * @param path
+ * @returns
+ */
+const imagePath = (path: string) => {
+  return new Promise<HTMLImageElement | null>((resolve) => {
+    if (path === "") {
+      resolve(null);
+    }
+    const img = new Image(400, 400);
+    img.src = path;
+    img.onload = (e) => {
+      console.log(e);
+      resolve(img);
+    };
+  });
+};
+
+/**
+ * Function to get svg
+ * @param modules
+ * @returns svg
+ */
+const resolveImportGLobModule = async (
+  modules: Record<string, ImportModuleFunction>
+) => {
+  const imports = Object.values(modules).map((importFn) => importFn());
+  const loadedModules = await Promise.all(imports);
+
+  return loadedModules.map((module) => module.default);
+};
+
 const App: Component = () => {
-  const [headImages, setHeadImages] = createSignal([]);
-  const [eyebowImages, setEyebowImages] = createSignal([]);
-  const [eyeImages, setEyeImages] = createSignal([]);
-  const [mouthImages, setMouthImages] = createSignal([]);
-  const [detailImages, setDetailImages] = createSignal([]);
+  const [images, setImages] = createSignal({
+    head: [],
+    eyebow: [],
+    eye: [],
+    mouth: [],
+    detail: [],
+  });
+  const [selectedTab, setSelectedTab] = createSignal<EmojiSlice>("head");
+  const [selectedIndex, setSelectedIndex] = createSignal({
+    head: 0,
+    eyebow: 0,
+    eye: 0,
+    mouth: 0,
+    detail: 0,
+  });
 
-  // selected number of sources array
-  const [selectedHead, setSelectedHead] = createSignal(0);
-  const [selectedEye, setSelectedEye] = createSignal(0);
-  const [selectedMouth, setSelectedMouth] = createSignal(0);
-  const [selectedEyebow, setSelectedEyebow] = createSignal(0);
-  const [selectedDetail, setSelectedDetail] = createSignal(0);
-
-  // selected image of the selected one
-  const [selectedHeadImage, setSelectedHeadImage] = createSignal("");
-  const [selectedEyeImage, setSelectedEyeImage] = createSignal("");
-  const [selectedMouthImage, setSelectedMouthImage] = createSignal("");
-  const [selectedEyebowImage, setSelectedEyebowImage] = createSignal("");
-  const [selectedDetailImage, setSelectedDetailImage] = createSignal("");
+  const selectedImage = () => {
+    return {
+      head: images().head[selectedIndex().head],
+      eyebow: images().eyebow[selectedIndex().eyebow],
+      eye: images().eye[selectedIndex().eye],
+      mouth: images().mouth[selectedIndex().mouth],
+      detail: images().detail[selectedIndex().detail],
+    };
+  };
 
   const loadImage = async () => {
     // bulk import svg
-    const headModules = await import.meta.glob("./assets/head/*.svg");
-    const eyebowsModules = await import.meta.glob("./assets/eyebows/*.svg");
-    const eyesModules = await import.meta.glob("./assets/eyes/*.svg");
-    const mouthModules = await import.meta.glob("./assets/mouth/*.svg");
-    const detailsModules = await import.meta.glob("./assets/details/*.svg");
+    const headModules = import.meta.glob<SvgImageModule>("./assets/head/*.svg");
+    const eyebowModules = import.meta.glob<SvgImageModule>(
+      "./assets/eyebows/*.svg"
+    );
+    const eyeModules = import.meta.glob<SvgImageModule>("./assets/eyes/*.svg");
+    const mouthModules = import.meta.glob<SvgImageModule>(
+      "./assets/mouth/*.svg"
+    );
+    const detailModules = import.meta.glob<SvgImageModule>(
+      "./assets/details/*.svg"
+    );
 
-    // value is a import function so to get the svgs should loop to execute them
-    const headValues = Object.values(headModules).map((m) => m());
-    const eyebowsValues = Object.values(eyebowsModules).map((m) => m());
-    const eyesValues = Object.values(eyesModules).map((m) => m());
-    const mouthValues = Object.values(mouthModules).map((m) => m());
-    const detailsValues = Object.values(detailsModules).map((m) => m());
+    const _headImages = await resolveImportGLobModule(headModules);
+    const _eyebowImages = await resolveImportGLobModule(eyebowModules);
+    const _eyeImages = await resolveImportGLobModule(eyeModules);
+    const _mouthImages = await resolveImportGLobModule(mouthModules);
+    const _detailImages = await resolveImportGLobModule(detailModules);
 
-    // remember the bulk import is a Promise so headValue is Promise too
-    const _headImages = await Promise.all(headValues);
-    const _eyebowImages = await Promise.all(eyebowsValues);
-    const _eyeImages = await Promise.all(eyesValues);
-    const _mouthImages = await Promise.all(mouthValues);
-    const _detailImages = await Promise.all(detailsValues);
+    setImages({
+      head: _headImages,
+      eyebow: [..._eyebowImages],
+      eye: [..._eyeImages],
+      mouth: [..._mouthImages],
+      detail: [..._detailImages],
+    });
 
-    setHeadImages(_headImages);
-    setEyebowImages(_eyebowImages);
-    setEyeImages(_eyeImages);
-    setMouthImages(_mouthImages);
-    setDetailImages(_detailImages);
+    getRandomEmojiCombination();
   };
 
-  loadImage();
+  onMount(() => {
+    loadImage();
+  });
 
-  const handleClickHead = (i) => {
-    setSelectedHead(i);
-    setSelectedHeadImage(headImages()[i()].default);
-  };
-  const handleClickEye = (i) => {
-    setSelectedEye(i);
-    setSelectedEyeImage(eyeImages()[i()].default);
-  };
-  const handleClickMouth = (i) => {
-    setSelectedMouth(i);
-    setSelectedMouthImage(mouthImages()[i()].default);
-  };
-  const handleClickEyebow = (i) => {
-    setSelectedEyebow(i);
-    setSelectedEyebowImage(eyebowImages()[i()].default);
-  };
-  const handleClickDetail = (i) => {
-    setSelectedMouth(i);
-    setSelectedDetailImage(detailImages()[i()].default);
+  createEffect(() => {
+    const headPath = selectedImage().head;
+    const eyebowPath = selectedImage().eyebow;
+    const eyePath = selectedImage().eye;
+    const mouthPath = selectedImage().mouth;
+    const detailPath = selectedImage().detail;
+    Promise.all([
+      imagePath(headPath),
+      imagePath(eyebowPath),
+      imagePath(eyePath),
+      imagePath(mouthPath),
+      imagePath(detailPath),
+    ]).then((images) => {
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      images.forEach((img) => {
+        img && ctx.drawImage(img, 0, 0, canvasSize, canvasSize);
+      });
+      canvas.classList.add("animation");
+      setTimeout(() => {
+        canvas.classList.remove("animation");
+      }, 500);
+    });
+  });
+
+  const handleSelectItem = ({ tab, index }: { tab: string; index: number }) => {
+    setSelectedIndex({ ...selectedIndex(), [tab]: index });
   };
 
-  const randomInt = (min, max) => {
+  const randomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const getRandomEmojiCombination = () => {};
+  // random emoji
+  const getRandomEmojiCombination = () => {
+    const randomIndexes = {
+      head: randomInt(0, images().head.length - 1),
+      eye: randomInt(0, images().eye.length - 1),
+      eyebow: randomInt(0, images().eyebow.length - 1),
+      mouth: randomInt(0, images().mouth.length - 1),
+      detail: randomInt(0, images().detail.length - 1),
+    };
+    setSelectedIndex(randomIndexes);
+  };
+
+  // export image
+  const exportImage = (blob: Blob) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `emoji_${Date.now()}`;
+    a.click();
+  };
+
+  // export svg
+  const toSvgBlob = async () => {
+    const parser = new DOMParser();
+    const documents = await Promise.all(
+      Object.values(selectedImage()).map((image) =>
+        fetch(image).then((response) => response.text())
+      )
+    );
+    const svg = (
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {documents.flatMap((document) => [
+          ...parser.parseFromString(document, "image/svg+xml").documentElement
+            .childNodes,
+        ])}
+      </svg>
+    ) as HTMLElement;
+    return new Blob([svg.outerHTML], { type: "image/svg+xml" });
+  };
 
   return (
     <>
-      <div mx-10 mt-10>
-        <h1 text-3xl font-bold mb-5>
-          Emoji Maker
-        </h1>
-        {/* head */}
-        <h2 mt-4 text-xl font-bold>
-          face
-        </h2>
-        <div flex="~ row wrap" gap-2>
-          <For each={headImages()}>
-            {(item, index) => (
-              <SelectButton>
-                <img
-                  onClick={[handleClickHead, index]}
-                  src={item.default}
-                  alt=""
-                />
-              </SelectButton>
-            )}
-          </For>
+      <main
+        flex="~ col"
+        items-center
+        justify-center
+        gap-4
+        max-w="65ch"
+        px-6
+        py-12
+        mx-auto
+        bg-white
+        rounded-lg
+        bg-op-80
+        shadow="2xl black/10"
+        dark:bg-dark
+        md:px-24
+      >
+        <div
+          flex
+          items-center
+          justify-center
+          w="200px"
+          h="200px"
+          border-2
+          border-neutral-400
+          border-op-20
+          rounded-2xl
+        >
+          <canvas
+            ref={canvas}
+            width={canvasSize}
+            height={canvasSize}
+            w="160px"
+            h="160px"
+            class="animation"
+          ></canvas>
         </div>
-        {/* eyebow */}
-        <h2 mt-8 text-xl font-bold>
-          Eyebow
-        </h2>
-        <div flex="~ row wrap" gap-2>
-          <For each={eyebowImages()}>
-            {(item, index) => (
-              <SelectButton>
-                <img
-                  onClick={[handleClickEyebow, index]}
-                  src={item.default}
-                  alt=""
-                />
-              </SelectButton>
-            )}
-          </For>
+        <div flex h-12 gap-2>
+          <div
+            flex
+            items-center
+            justify-center
+            w-12
+            rounded-full
+            bg-neutral-100
+            dark:bg-neutral-600
+            text-black
+            dark:text-white
+            cursor-pointer
+            transition-colors
+            hover="bg-violet-200 dark:bg-violet-400"
+            onClick={getRandomEmojiCombination}
+          >
+            <div i-material-symbols-refresh text-2xl />
+          </div>
+          <div
+            inline-flex
+            px-3
+            items-center
+            gap-1
+            rounded-full
+            bg-neutral-100
+            dark:bg-neutral-600
+            text-black
+            dark:text-white
+            cursor-pointer
+            transition-colors
+            hover="bg-violet-200 dark:bg-violet-400"
+            onClick={() => canvas.toBlob(exportImage)}
+          >
+            <div i-material-symbols-download-rounded text-2xl />
+            <span font-bold mr-1>
+              Export PNG
+            </span>
+          </div>
+          <div
+            inline-flex
+            px-3
+            items-center
+            gap-1
+            rounded-full
+            bg-neutral-100
+            dark:bg-neutral-600
+            text-black
+            dark:text-white
+            cursor-pointer
+            transition-colors
+            hover="bg-violet-200 dark:bg-violet-400"
+            onClick={() => toSvgBlob().then(exportImage)}
+          >
+            <div i-material-symbols-download-rounded text-2xl />
+            <span font-bold mr-1>
+              Export SVG
+            </span>
+          </div>
         </div>
-        {/* eye */}
-        <h2 mt-8 text-xl font-bold>
-          Eye
-        </h2>
-        <div flex="~ row wrap" gap-2>
-          <For each={eyeImages()}>
-            {(item, index) => (
-              <SelectButton>
-                <img
-                  onClick={[handleClickEye, index]}
-                  src={item.default}
-                  alt=""
-                />
-              </SelectButton>
-            )}
-          </For>
+        <div w-full mt-4>
+          <header
+            flex
+            flex-wrap
+            items-center
+            gap-3
+            p-4
+            border-b
+            border-neutral-400
+            border-op-20
+            justify-center
+          >
+            <For each={tabs}>
+              {(item, index) => (
+                <div
+                  flex
+                  items-center
+                  justify-center
+                  h-16
+                  w-16
+                  rounded-lg
+                  cursor-pointer
+                  transition-colors
+                  hover="bg-violet-200 dark:bg-violet-200"
+                  class={
+                    selectedTab() === item
+                      ? "bg-violet-200 dark:bg-violet-200"
+                      : "bg-neutral-100 dark:bg-neutral-600"
+                  }
+                  onClick={() => setSelectedTab(item)}
+                >
+                  <Show when={selectedImage()[item]}>
+                    <img
+                      src={selectedImage()[item]}
+                      alt={selectedTab() + index()}
+                      h-12
+                      w-12
+                    ></img>
+                  </Show>
+                </div>
+              )}
+            </For>
+          </header>
+          <main p-4>
+            <div flex="~ wrap" gap-2 justify-center>
+              <Switch>
+                <For each={Object.keys(images())}>
+                  {(tab: EmojiSlice) => (
+                    <Match when={tab === selectedTab()}>
+                      <For each={images()[tab]}>
+                        {(item, index) => (
+                          <SelectButton
+                            highlight={() =>
+                              index() === selectedIndex()[selectedTab()]
+                            }
+                            onClick={[
+                              handleSelectItem,
+                              { tab: selectedTab(), index: index() },
+                            ]}
+                          >
+                            <Show when={item}>
+                              <img
+                                src={item}
+                                alt={selectedTab() + index()}
+                                h-10
+                                w-10
+                              ></img>
+                            </Show>
+                          </SelectButton>
+                        )}
+                      </For>
+                    </Match>
+                  )}
+                </For>
+              </Switch>
+            </div>
+          </main>
         </div>
-        {/* mouth */}
-        <h2 mt-8 text-xl font-bold>
-          Mouth
-        </h2>
-        <div flex="~ row wrap" gap-2>
-          <For each={mouthImages()}>
-            {(item, index) => (
-              <SelectButton>
-                <img
-                  onClick={[handleClickMouth, index]}
-                  src={item.default}
-                  alt=""
-                />
-              </SelectButton>
-            )}
-          </For>
-        </div>
-        {/* detail */}
-        <h2 mt-8 text-xl font-bold>
-          Detail
-        </h2>
-        <div flex="~ row wrap" gap-2>
-          <For each={detailImages()}>
-            {(item, index) => (
-              <SelectButton>
-                <img
-                  onClick={[handleClickDetail, index]}
-                  src={item.default}
-                  alt=""
-                />
-              </SelectButton>
-            )}
-          </For>
-        </div>
-      </div>
-
-      <div my-8 border h-40 mx-10>
-        <img class="absolute w-24 h-24" src={selectedHeadImage()} alt="" />
-        <img class="absolute w-24 h-24" src={selectedEyeImage()} alt="" />
-        <img class="absolute w-24 h-24" src={selectedMouthImage()} alt="" />
-        <img class="absolute w-24 h-24" src={selectedEyebowImage()} alt="" />
-        <img class="absolute w-24 h-24" src={selectedDetailImage()} alt="" />
-      </div>
-
-      <button></button>
+      </main>
     </>
   );
 };
--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -1,15 +1,17 @@
 export default (props) => {
   const slot = props.children;
+  const isHighLight = props.HighLight;
+
   return (
     <div
       bg-gray-100
       p-2
       rounded-md
       border
-      border-transparent
       transition-colors
       cursor-pointer
       hover="bg-red-100 border border-red-400"
+      class={isHighLight ? "border-red-400 bg-red-100" : "border-transparent"}
     >
       {slot}
     </div>
